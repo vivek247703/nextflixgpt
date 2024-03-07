3956df72e767a30b2baf2e1e48e5e364970b48d4
Vivek Hariyan <vivekhariyan09@gmail.com>
2024-03-07 19:19:57 +0530

    Login and Sign Up Form Frontend Completed

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='absolute w-36 px-4 py-4 bg-gradient-to-b from-black z-10'>
-      <img className='w-44' src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'
+    <div className='absolute w-64 px-4 py-4 bg-gradient-to-b from-gray-950 z-10'>
+      <img className='w-64 h-20' src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'
       alt=''></img>
     </div>
   )
--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
+import { alert } from '../Utils/Constants';
 
 const Login = () => {
+  
+  const [Login , setLogin]=useState(true);
+  const SignInForm=()=>{
+    setLogin(!Login);
+  }
+  
   return (
     <div>
       <Header/>
@@ -9,11 +16,13 @@ const Login = () => {
       <img src='https://assets.nflxext.com/ffe/siteui/vlv3/93da5c27-be66-427c-8b72-5cb39d275279/94eb5ad7-10d8-4cca-bf45-ac52e0a052c0/IN-en-20240226-popsignuptwoweeks-perspective_alpha_website_small.jpg'
       alt=''></img>
       </div>
-      <form className='w-3/12 bg-black absolute p-12 my-36 mx-auto right-0 left-0 text-white'>
-        <h1 className='text-white text-xl font-bold'>Sign In</h1>
-        <input className='p-2 m-2' type='text' placeholder='Enter a email/Phone number'></input>
-        <input className='p-2 m-2' type='password' placeholder='Enter apassword'></input>
-        <button className='p-2 m-2 bg-red-600 rounded-lg' type='submit'>Sign Up</button>
+      <form className='w-96 bg-black absolute p-12 my-36 mx-auto right-0 left-0 text-white bg-opacity-80'>
+        <h1 className='text-white text-4xl'>{Login? "Sign In" :"Sign Up"}</h1>
+        {!Login && <input className='p-2  mt-6 w-72 rounded-md bg-gray-700' type='text' placeholder='Enter a Name'></input>}
+        <input className='p-2 mt-6 w-72 rounded-md bg-gray-700' type='text' placeholder='Enter a email/Phone number'></input>
+        <input className='p-2  mt-6 w-72 rounded-md bg-gray-700' type='password' placeholder='Enter password'></input>
+        <button className='p-2 mt-8 w-72 bg-red-600 rounded-lg' type='submit'>{Login? "Sign In" :"Sign Up"}</button>
+        <p className='mt-9 text-gray-500'>{Login? "New to Netflix?" :"Already a user?"}<span className='font-semibold text-white cursor-pointer p-1' onClick={SignInForm}>{Login? "Sign Up" :"Sign In"}</span></p>
       </form>
     </div>
   )
